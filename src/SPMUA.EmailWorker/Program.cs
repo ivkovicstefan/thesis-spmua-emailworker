ebc4ba1262fc05a2d171e2eb77e55e93a38a512b
Stefan Ivković <ivkovics@outlook.com>
2024-05-27 22:58:35 +0200

    chore: Add appsettings configuration

--- src/SPMUA.EmailWorker/Program.cs
+++ src/SPMUA.EmailWorker/Program.cs
@@ -5,6 +5,14 @@ IHost host = Host.CreateDefaultBuilder(args)
     {
         services.AddHostedService<Worker>();
     })
+    .ConfigureAppConfiguration((hostContext, configBuilder) =>
+    {
+        configBuilder
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{hostContext.HostingEnvironment.EnvironmentName}.json", optional: true, reloadOnChange: true)
+            .Build();
+    })
     .Build();
 
 await host.RunAsync();
--- src/SPMUA.EmailWorker/Worker.cs
+++ src/SPMUA.EmailWorker/Worker.cs
@@ -1,20 +1,26 @@
+using SPMUA.EmailWorker.Models.Config;
+
 namespace SPMUA.EmailWorker
 {
     public class Worker : BackgroundService
     {
         private readonly ILogger<Worker> _logger;
+        private readonly IConfiguration _configuration;
 
-        public Worker(ILogger<Worker> logger)
+        public Worker(ILogger<Worker> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
+            WorkerConfig workerConfig = _configuration.GetSection("WorkerConfig").Get<WorkerConfig>();
+
             while (!stoppingToken.IsCancellationRequested)
             {
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-                await Task.Delay(1000, stoppingToken);
+                await Task.Delay(workerConfig.Delay, stoppingToken);
             }
         }
     }
